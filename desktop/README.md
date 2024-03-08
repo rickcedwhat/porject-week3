358ecc57389778890c509c86c8865c34d346e679
Cedrick Catalan <ccata002@gmail.com>
2024-03-08 16:13:03 -0500

    first commit (also last commit)

---
+++ desktop/README.md
@@ -0,0 +1,1 @@
+ <a href='https://pngtree.com/freebackground/windows-desktop-wallpaper-showing-a-green-grassy-area_3418806.html'>free background photos from pngtree.com/</a>
--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const tabs = document.querySelectorAll('[id$=tab]');
-console.log({tabs})
 tabs.forEach(tab=>tab.addEventListener('click',(e)=>{
     const name = e.target.id.split("-")[0];
-    const activeSection = document.querySelector('active');
+
+    const activeSection = document.querySelector('[id$=section].active');
     activeSection.classList.remove('active');
+    
     const matchingSection = document.querySelector(`#${name}-section`)
-    matchingSection.classList.add('active')
+    matchingSection.classList.add('active');
+    
+    const activeTab = document.querySelector('[id$=tab].active');
+    activeTab.classList.remove('active');
+
+    tab.classList.add('active');
+    
 }))
